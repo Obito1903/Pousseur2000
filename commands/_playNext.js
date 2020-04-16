98cfc655a74a2c06b300541921ef547785be9df7
Samuel Rodrigues <samuel.rodrigues@eisti.fr>
2020-04-16 21:50:52 +0200

    added leave and updated join command

--- commands/_playNext.js
+++ commands/_playNext.js
@@ -2,20 +2,19 @@ exports.run = (client, message, args) => {
 
     try {
         const guild = client.guildsList.get(message.guild.id);
-        let song = {};
-        //console.log(guild.queue[0]);
         if (guild.queue.length > 0) {
-            song = guild.queue[0];
+            const song = guild.queue[0];
             guild.queue.shift();
+
+            if (song) return client.commands.get('_playNow').run(client, message, song);
+            client.commands.get('stop').run(client, message, args);
+            /*
+            if (this.autoLeaveIn !== 0) {
+                this.timeout = setTimeout(() => this.disconnectVoiceConnection(msg), this.autoLeaveIn);
+            }
+            */
+            message.channel.send('Queue is empty, playback finished.');
         }
-        if (song) return client.commands.get('_playNow').run(client, message, song);
-        client.commands.get('stop').run(client, message, args);
-        /*
-        if (this.autoLeaveIn !== 0) {
-            this.timeout = setTimeout(() => this.disconnectVoiceConnection(msg), this.autoLeaveIn);
-        }
-        */
-        message.channel.send('Queue is empty, playback finished.');
     } catch (err) {
         console.log('err' + err)
         client.voice.connections.forEach(conn => {
--- commands/join.js
+++ commands/join.js
@@ -1,7 +1,11 @@
 exports.run = (client, message, args) => {
-    return new Promise((resolve, reject) => {
-        const voiceChannel = message.member.voice.channel;
-        if (!voiceChannel || voiceChannel.type !== 'voice') return message.reply('I couldn\'t connect to your voice channel...');
-        voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
-    });
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) return message.channel.send('Must be in a voice channel.');
+    if (!message.guild) throw new Error('Unable to find discord server.');
+    const voiceConnection = client.voice.connections.find(val => val.channel.guild.id === message.guild.id);
+    if (!voiceConnection || force) {
+        if (voiceChannel && voiceChannel.joinable) return voiceChannel.join();
+        throw new Error('Unable to join your voice channel.');
+    }
+    message.channel.send('Joining voice channel.');
 }
