d2e1c3f430c8292ffef624358acf5fa4dbb8960a
Samuel Rodrigues <samuel.rodrigues@eisti.fr>
2020-04-16 22:28:17 +0200

    removed force variable

--- commands/join.js
+++ commands/join.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     if (!voiceChannel) return message.channel.send('Must be in a voice channel.');
     if (!message.guild) throw new Error('Unable to find discord server.');
     const voiceConnection = client.voice.connections.find(val => val.channel.guild.id === message.guild.id);
-    if (!voiceConnection || force) {
+    if (!voiceConnection) {
         if (voiceChannel && voiceChannel.joinable) return voiceChannel.join();
         throw new Error('Unable to join your voice channel.');
     }
