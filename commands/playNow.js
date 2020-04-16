96c29eb78cee46b7ee7a70665c0d741511d24f12
Samuel Rodrigues <samuel.rodrigues@eisti.fr>
2020-04-16 16:31:33 +0200

    dhzjk

---
+++ commands/playNow.js
@@ -0,0 +1,25 @@
+exports.run = async (client, message, args) => {
+    try {
+        const guild = client.guilds.get(message.guild.id);
+        const voiceConnection = client.voice.connections.find(val => val.channel.guild.id === message.guild.id);
+
+        const ytOptions = {
+            filter: 'audioonly',
+            quality: 'highestaudio',
+            highWaterMark: 1 << 25
+        };
+
+        const dOptions = {
+            volume: guild.volume / 100,
+            bitrate: client.config.bitRate,
+            passes: 3,
+            seek,
+            type: 'opus',
+            highWaterMark: 1
+        }
+
+        guild.audioDispatcher = voiceConnection.play(ytdl(args[0], ytOptions), dOptions);
+    } catch (err) {
+        console.log('Erreur playNow.js' + err);
+    }
+}
